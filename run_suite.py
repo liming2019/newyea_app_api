cfe90ce3c14b230b188ade8decb69c982741c4f3
tester <2864489626@qq.com>
2023-06-29 09:04:06 +0800

    修改测试报告文件名

--- run_suite.py
+++ run_suite.py
@@ -7,7 +7,8 @@ from script.Login import TestLogin
 suite = unittest.TestSuite()
 suite.addTests(unittest.makeSuite(TestLogin))
 
-report_file = app.base_path + '/report/report_{}.html'.format(time.strftime("%Y-%m-%d %H-%M-%S"))
+# report_file = app.base_path + '/report/report_{}.html'.format(time.strftime("%Y-%m-%d %H-%M-%S"))
+report_file = app.base_path + '/report/report.html'
 with open(report_file, 'wb') as f:
     runner = HTMLTestRunner(f, title='新页充电app接口测试')
     runner.run(suite)
